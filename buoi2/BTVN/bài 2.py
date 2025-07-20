af84675f0c18fcb9f921e22f4150cf503322ec9d
ntdat1602 <ntdat160206@gmail.com>
2025-07-21 00:03:10 +0700

    btvn

---
+++ buoi2/BTVN/bài 2.py
@@ -0,0 +1,17 @@
+# Giả sử bạn là kế toán cho một công ty. Bạn cần viết một chương trình để nhập lương nhân viên, tính thuế thu nhập và lương ròng (số tiền lương thực sự mà nhân viên đó nhận được). Với các thông số sau (mình chỉ đưa ra các con số này cho dễ tính toán):
+# - 30% thuế thu nhập nếu lương là 15 triệu
+# - 20% thuế thu nhập nếu lương từ 7 đến 15 triệu
+# - 10% thuế thu nhập nếu lương dưới 7 triệu
+
+luong = int ( input ( " Nhập lương : ")) 
+if luong == 15000000 : 
+    thue = luong * 0.3 
+elif 7000000 <= luong <= 15000000 : 
+    thue = luong * 0.2 
+elif luong < 7000000 : 
+    thue = luong * 0.1 
+
+luong_rong = luong - thue 
+print ( " Lương : " , luong ) 
+print ( " Thuế thu nhập : " , thue ) 
+print ( " Lương ròng : " , luong_rong )
