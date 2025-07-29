bc9a22df8fe8b2c7dc2cb13169099fed743d35af
ntdat1602 <ntdat160206@gmail.com>
2025-07-29 22:16:00 +0700

    python

---
+++ BUỔI 3/trenlop.py
@@ -0,0 +1,28 @@
+list_a = [1, 2, 'a', 2.3, 4, 5, 'b']
+print (len(list_a))
+
+# #Method 1:
+# for i in range (len(list_a)):
+#         print(list_a[i], end = ' ') 
+
+# #Method 2:
+# for x in list_a:
+#         print(x, end = ', ')
+
+# a = [1, 2, 3, 4, 5]
+# for x in range(len(a)):
+#     if x < 4:
+#         print(a[x], end = '')
+#     else:
+#         print(a[x], sep = '\n', end = '#')  
+
+# list1 = [4, 5, 6]
+# list1.reverse()
+# print(list1)
+
+# list2 = [4, 1, 8, 3]
+# list2.sort(reverse = True)
+# print(list2)
+
+
+
